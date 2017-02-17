5ed845c75d6de26747deb94f73b6410616a4d65b
SirOussama <oussama_amairi@hotmail.com>
2017-02-17 14:20:06 +0100

    Add files via upload

---
+++ asp_netAPP/Startup.cs
@@ -0,0 +1,19 @@
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+namespace ConsoleApplication
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+           app.UseDefaultFiles();
+        app.UseStaticFiles();
+        //app.Run(
+       //     context => {return context.Response.WriteAsync("Hello from the world");}
+      //  );
+        
+        }
+    }
+}
